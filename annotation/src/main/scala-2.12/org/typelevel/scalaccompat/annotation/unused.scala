4c78726b31564d37763b36f7fc51bae6ba38fe33
satorg <satorg@gmail.com>
2022-10-21 00:05:29 -0700

    add custom `@nowarn*` and `@unused` annotations

---
+++ annotation/src/main/scala-2.12/org/typelevel/scalaccompat/annotation/unused.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2022 Typelevel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.typelevel.scalaccompat.annotation
+
+import scala.annotation.meta
+
+/** Mimics `@unused` from Scala v2.13 and v3 libraries. It reproduces the same singature of the
+  * original annotation, but inerits to `@nowarn` rather than `ConstantAnnotation`.
+  *
+  * @param message
+  *   currently not in use
+  * @see
+  *   [[https://github.com/scala/scala/blob/2.13.x/src/library/scala/annotation/unused.scala]]
+  */
+@meta.getter @meta.setter
+@nowarn(
+  "cat=other&msg=Implementation restriction: subclassing ClassfileAnnotation does not\nmake your annotation visible at runtime."
+)
+class unused(message: String) extends nowarn("cat=unused") {
+  def this() = this("")
+}
