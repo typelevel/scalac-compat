c378451b5ba6eb75b36a608423b8d3fb5f118b66
Brian Holt <bholt@dwolla.com>
2026-06-02 15:56:46 -0500

    Use ??? to stub ambiguous NotGivens

--- scala2-notgiven-compat/src/main/scala-2/org/typelevel/scalaccompat/scala/util/NotGiven.scala
+++ scala2-notgiven-compat/src/main/scala-2/org/typelevel/scalaccompat/scala/util/NotGiven.scala
@@ -24,6 +24,6 @@ object NotGiven {
   implicit def notGiven[A]: NotGiven[A] = new NotGiven[A] {}
 
   @implicitAmbiguous("Found an implicit value of type ${A}, so NotGiven[${A}] cannot be satisfied.")
-  implicit def amb1[A](implicit @unused ev: A): NotGiven[A] = null
-  implicit def amb2[A](implicit @unused ev: A): NotGiven[A] = null
+  implicit def amb1[A](implicit @unused ev: A): NotGiven[A] = ???
+  implicit def amb2[A](implicit @unused ev: A): NotGiven[A] = ???
 }
